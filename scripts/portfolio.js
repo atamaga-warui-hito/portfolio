c655f44e7642ae4bd449d8e805a4e4c9098ec258
Biacco42 <biacco42@gmail.com>
2021-03-07 19:13:11 +0900

    Add content loading

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,5 +1,47 @@
 'use strict';
 
-window.onload = (event) => {
-    console.log(event)
+let state
+
+function State(contentsList, stateEventHandler) {
+    this.contentsList = contentsList
+    this.appendingIndex = 0
+    inflateContents(contentsList, 4)
+
+    function inflateContents(contentsList, columnCount) {
+        contentsList.forEach((contentURL, index) => {
+            let addColumnIndex = index % columnCount
+
+            getContentDOM(contentURL)
+                .then(content => {
+                    let column = document.getElementsByClassName("column")[addColumnIndex]
+                    column.appendChild(content)
+                })
+        })
+    }
+
+    function getContentDOM(url) {
+        return fetch(url)
+            .then(response => {
+                return response.json()
+            }).then(responseJSON => {
+                let contentNode = document.createElement("a")
+                contentNode.setAttribute("href", responseJSON.externalLink)
+                contentNode.textContent = responseJSON.title
+                return contentNode
+            })
+    }
+}
+
+window.onload = (_) => {
+    fetch("../contents-list.json")
+        .then(response => {
+            return response.json()
+        })
+        .then(responseJSON => {
+            onContentsListReceived(responseJSON)
+        })
+}
+
+function onContentsListReceived(contentsList) {
+    state = new State(contentsList)
 }
