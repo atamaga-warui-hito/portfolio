7cbd5c9dffa640f44bb64d68f780459164f9052a
Biacco42 <biacco42@gmail.com>
2021-03-07 19:51:38 +0900

    Add contents inflate logic

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -24,16 +24,41 @@ function State(contentsList, stateEventHandler) {
             .then(response => {
                 return response.json()
             }).then(responseJSON => {
-                let contentNode = document.createElement("a")
-                contentNode.setAttribute("href", responseJSON.externalLink)
-                contentNode.textContent = responseJSON.title
+                // let imageSource = "images/360x360.png"
+                // if (responseJSON.hasOwnProperty("thumbnail")) {
+                //     imageSource = responseJSON.thumbnail
+                // }
+                let imageSource = retrieveOrDefault(responseJSON, "thumbnail", "images/360x360.png")
+                let image = document.createElement("img")
+                image.setAttribute("src", imageSource)
+                image.setAttribute("class", "thumbnail")
+
+                let thumbnail = document.createElement("div")
+                thumbnail.appendChild(image)
+
+                let titleString = retrieveOrDefault(responseJSON, "title", "")
+                let title = document.createElement("h1")
+                title.textContent = titleString
+
+                let descriptionString = retrieveOrDefault(responseJSON, "description", "")
+                let description = document.createElement("p")
+                description.textContent = descriptionString
+
+                let label = document.createElement("div")
+                label.appendChild(title)
+                label.appendChild(description)
+
+                let contentNode = document.createElement("div")
+                contentNode.appendChild(thumbnail)
+                contentNode.appendChild(label)
                 return contentNode
             })
     }
+
 }
 
 window.onload = (_) => {
-    fetch("../contents-list.json")
+    fetch("contents-list.json")
         .then(response => {
             return response.json()
         })
@@ -44,4 +69,12 @@ window.onload = (_) => {
 
 function onContentsListReceived(contentsList) {
     state = new State(contentsList)
+}
+
+function retrieveOrDefault(object, key, defaultValue) {
+    if (object.hasOwnProperty(key)) {
+        return object[key]
+    } else {
+        return defaultValue
+    }
 }
